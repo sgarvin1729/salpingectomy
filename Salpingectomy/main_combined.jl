b47c562ecb73a9e1309d08b4e75fc3044d6f00f6
eulerbasil1707@gmail.com <eulerbasil1707@gmail.com>
2026-03-12 15:45:08 -0500

    bumping non opp step size from 2 years to 5 years

--- Salpingectomy/main_combined.jl
+++ Salpingectomy/main_combined.jl
@@ -342,7 +342,7 @@ for failure_rate in [0.15, 0.25, 0.35, 0.45, 0.55, 0.65, 0.75]
                 end
             end
             
-            age += 2
+            age += 5
             
         end
 
