12e664607f651baee8460a2dd0b30a4d075b272a
eulerbasil1707@gmail.com <eulerbasil1707@gmail.com>
2026-04-02 17:27:34 -0500

    0.1% per year

--- Salpingectomy/main_combined.jl
+++ Salpingectomy/main_combined.jl
@@ -114,7 +114,7 @@ twentypercenteverytenyears = zeros(1080)
 
 for k in 1:69
     idx = 241 + 12*k
-    twentypercenteverytenyears[idx] = min(0.002*k, 0.1)
+    twentypercenteverytenyears[idx] = min(0.001*k, 0.1)
 end
 
 uniformthirtysixtynormalfortyfiveseven = zeros(1080)
